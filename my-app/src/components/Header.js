bff3f1320fb391bfb4a354284b19a6459471c9b5
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-28 16:57:05 -0300

    Header

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div style={{padding: "10px 50px", background: "black"}}>
+            <div style={{color: "white"}}>Pizza Customization</div>
+        </div>
+    );
+}
