0abafb169251305175ed72e3d7c1ca24536ef685
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-30 16:30:37 -0300

    Image Placement

--- my-app/src/components/Customize.js
+++ my-app/src/components/Customize.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Cheese from "../assets/BaseCheese.png";
+import Base from "../assets/PizzaBase.png";
+import Olive from "../assets/Olive.png";
+import Pineapple from "../assets/Pineapple.png";
+import Mushroom from "../assets/Mushroom.png";
+import Basil from "../assets/Basil.png";
+import Tomato from "../assets/Tomato.png";
 
 export default function Customize() {
     return(
-        <div>
-            <div style={{border: "2px solid dark"}}>
-                Image
+        <div style={{display: "flex"}}>
+            <div style={{border: "2px solid black", flex: 1}}>
+                <div style={{minHeight: 500}}>
+                    <img src={Base} alt="Pizza Base" />
+                </div>
             </div>
-            <div style={{border: "2px solid dark"}}>
+            <div style={{border: "2px solid black", flex: 1}}>
                 Checkbox
             </div>
         </div>
