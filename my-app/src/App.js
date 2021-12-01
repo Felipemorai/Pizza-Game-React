8419122ea2d7ecb9eeac123e497cb8ae6f2c6d0b
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-01 16:11:40 -0300

    useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Customize from "./components/Customize";
 
 function App() {
+    const [ingredients, setIngredients] = useState ({
+        basil: false,
+        cheese: false,
+        mushroom: false,
+        olive: false,
+        pineapple: false,
+        tomato: false,
+    });
+
     return (
     <div>
         <Header />
         <Router>
             <Switch>
                 <Route exact path="/">
-                    <Customize />
+                    <Customize ingredients={ingredients}/>
                 </Route>
                 <Route path="/checkout">
                     <h1>Checkout</h1>
--- my-app/src/components/Customize.js
+++ my-app/src/components/Customize.js
@@ -7,17 +7,13 @@ import Mushroom from "../assets/Mushroom.png";
 import Basil from "../assets/Basil.png";
 import Tomato from "../assets/Tomato.png";
 
-export default function Customize() {
+export default function Customize({ingredients}) {
     return(
-        <div style={{display: "flex"}}>
+        <div style={{display: "flex" }}>
+            {JSON.stringify(ingredients)}
             <div style={{border: "2px solid black", flex: 1}}>
                 <div style={{maxHeight: 500, maxWidth: 500, position:"relative"}}>
-                    <img
-                      src={Base}
-                      alt="Pizza Base" 
-                      height="100%" 
-                      width="100%" 
-                    />
+                   
                     <img
                       src={Cheese}
                       alt="Pizza Base" 
@@ -59,6 +55,12 @@ export default function Customize() {
                       height="100%" 
                       width="100%" 
                       className="ingredients" 
+                    />
+                    <img
+                      src={Base}
+                      alt="Pizza Base" 
+                      height="100%" 
+                      width="100%" 
                     />
                 </div>
             </div>
