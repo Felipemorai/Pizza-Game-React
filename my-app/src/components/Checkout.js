3f878616f9a1922fce4ce15417deeefeae853dcc
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-06 17:04:51 -0300

    setSuccess

--- my-app/src/components/Checkout.js
+++ my-app/src/components/Checkout.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 export default function Checkout({ ingredients }) {
+    const history = useHistory();
+    const [ success, setSuccess ] = useState(false);
+
     return (
     <div style={{ display: "flex" }}>
         <div>
             <div>
                 <h1>My Ingredients</h1>
-                {Object.keys(ingredients).map((ingredients)=>{
-                    return <p>
-                        {ingredients[0].toUpperCase()} 
-                        {ingredients.substring(1)}
-                    </p>;
+                {Object.keys(ingredients).map((ingredient)=>{
+                  return (
+                      ingredients[ingredient] && (
+                    <p>
+                        {ingredient[0].toUpperCase()} 
+                        {ingredient.substring(1)}
+                    </p>
+                    )
+                  );
                 })}
+                <button onClick={()=>setSuccess(true)}>
+                    Confirm
+                </button>
+                <button onClick={()=>}>
+                    Go Back
+                </button>
             </div>
         </div>
         <div>Success</div>
