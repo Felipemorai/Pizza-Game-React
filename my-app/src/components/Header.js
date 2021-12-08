5bdd2a37a955cde02874b7b7add49d6ef836047e
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-07 21:33:02 -0300

    Logo

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Logo from "../assets/pizzalogo.png"
 
 export default function Header() {
     return (
-        <div style={{padding: "10px 50px", background: "black"}}>
-            <div style={{color: "white"}}>Pizza Customization</div>
+        <div style={{
+            background: "#0d0628",
+            padding: "15px 50px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+        }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <img src={Logo} alt="pizzalogo" height="40px" />
+                <div style={{
+                    marginLeft: 20,
+                    fontSize: 30,
+                    color: "yellow",
+                    fontFamily: "Indie Flower,cursive",
+                }}>
+                    Pizza Customization
+                </div>
+            </div>
         </div>
     );
 }
